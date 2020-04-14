e662bd19f5643f4f08b0c95326ef27f071481ef7
NikkiNorberg <62884988+NikkiNorberg@users.noreply.github.com>
2020-04-14 15:22:47 +0200

    försökte fixa collission utan resultat.

---
+++ Assets/NikkiScripts/CheckIfHasFallen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIfHasFallen : MonoBehaviour
+{
+
+    public bool state = false;
+    public string tagOfOtherObject = "";
+
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.CompareTag("Lane"))
+        {
+            state = true;
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(state);
+ 
+    }
+}
